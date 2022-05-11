193e32c86c10fb0e562e1212f5031da74f9cf3c9
Vassa Master <kroserium@yandex.ru>
2022-05-11 17:53:14 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Basket vasyaBasket = new Basket();
+        vasyaBasket.add("Молоко", 88);
+        vasyaBasket.add("Хлеб", 40);
+
+        Basket petyaBasket = new Basket(500);
+        petyaBasket.add("Лопата", 280);
+        petyaBasket.add("Бочка",2900);
+
+        Basket mashaBasket = new Basket("Стол",5000);
+
+        vasyaBasket.print("Корзина Васи:");
+        System.out.println();
+        petyaBasket.print("Корзина Пети:");
+        System.out.println();
+        mashaBasket.print("Корзина Маши:");
+    }
+}
